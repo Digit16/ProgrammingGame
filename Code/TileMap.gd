90989fa9372e07624b287eacbbc1326111eab39b
Draw2Cards <drawkka@gmail.com>
2024-05-28 18:05:34 +0200

    hello_world

---
+++ Code/TileMap.gd
@@ -0,0 +1,16 @@
+extends TileMap
+
+var GridSize = 16
+var Dic = {}
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#for x in GridSize:
+		#for y in GridSize:
+			#set_cell(0, Vector2(x,y), 0, Vector2i(0,0),0)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
