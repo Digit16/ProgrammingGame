3313663a896b18b8ee34843aee277105662bb6b6
Draw2Cards <drawkka@gmail.com>
2024-06-10 10:59:10 +0200

    rotation

--- Code/characters/player_robot.gd
+++ Code/characters/player_robot.gd
@@ -1,10 +1,10 @@
 extends CharacterBody2D
 
 signal done
-signal  picked_up
+signal picked_up
 
 var targetPos = Vector2.ZERO
-var facing = Vector2.ZERO
+var facing = Vector2(0, 1) # Default direction facing down
 var prevPos = Vector2.ZERO
 var waiting = true
 var abort = false
@@ -24,26 +24,32 @@ func _ready():
 			print(line)
 	file.close()
 
-	
-
 func _process(_delta):
 	if abort:
 		return
 	
 	if waiting:
-		waiting = false;
-		var c = commands.pop_front()
-		if c == 'd':
-			_on_node_down()
-		if c == 'u':
-			_on_node_up()
-		if c == 'l':
-			_on_node_left()
-		if c == 'r':
-			_on_node_right()
-		if c == 'p':
-			_on_pick_up()
-		animation_tree.set("parameters/Walk/blend_position", facing)
+		waiting = false
+		if commands.size() > 0:
+			var c = commands.pop_front()
+			match c:
+				'd':
+					_on_node_down()
+				'u':
+					_on_node_up()
+				'l':
+					_on_node_left()
+				'r':
+					_on_node_right()
+				'p':
+					_on_pick_up()
+				'rc':
+					_on_node_rotate_clockwise()
+				'rcc':
+					_on_node_rotate_c_clockwise()
+			animation_tree.set("parameters/Walk/blend_position", facing)
+		else:
+			state_machine.travel("End") # Idle state if no commands are left
 	else:
 		self.position = self.position.lerp(targetPos, 4 * _delta)
 		var collision = move_and_slide()
@@ -56,37 +62,38 @@ func _process(_delta):
 		else:
 			if self.position.distance_to(targetPos) < 1.0: 
 				if commands.is_empty():
-					state_machine.travel("hit_down")
+					emit_signal("done")
 				waiting = true
 
 func _on_node_down():
-	facing = Vector2.ZERO
-	facing.y = 1
+	facing = Vector2(0, 1)
 	prevPos = targetPos
 	targetPos.y += 16
 
-
 func _on_node_left():
-	facing = Vector2.ZERO
-	facing.x = -1
+	facing = Vector2(-1, 0)
 	prevPos = targetPos
 	targetPos.x -= 16
 
-
 func _on_node_right():
-	#$Character/AnimationPlayer.play("walk_right")
-	facing = Vector2.ZERO
-	facing.x = 1
+	facing = Vector2(1, 0)
 	prevPos = targetPos
 	targetPos.x += 16
 
-
 func _on_node_up():
-	#$Character/AnimationPlayer.play("walk_up")
-	facing = Vector2.ZERO
-	facing.y = -1
+	facing = Vector2(0, -1)
 	prevPos = targetPos
 	targetPos.y -= 16
+
+func _on_node_rotate_clockwise():
+	facing = Vector2(facing.y, -facing.x)
+	print("Rotated clockwise. New facing:", facing)
+	animation_tree.set("parameters/Walk/blend_position", facing)
+
+func _on_node_rotate_c_clockwise():
+	facing = Vector2(-facing.y, facing.x)
+	print("Rotated counter-clockwise. New facing:", facing)
+	animation_tree.set("parameters/Walk/blend_position", facing)
 
 var removal_distance = 32
 
