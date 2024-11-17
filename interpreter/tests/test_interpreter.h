164f12226472781caaf13b9ed5767eb7bfbe8971
Wiktor Kubski <wiktor.kubski1@gmail.com>
2024-11-17 18:23:56 +0000

    Cleanup repo

--- interpreter/tests/test_interpreter.h
+++ interpreter/tests/test_interpreter.h
@@ -1,18 +1,16 @@
 #pragma once
 
-
-#include "tests/test_macros.h"
 #include "../CodeInterpreter.h"
+#include "tests/test_macros.h"
 
 namespace TestInterpreter {
 
+TEST_CASE("[Interpreter] 2+2")
+{
+    CodeInterpreter interpreter{};
+    interpreter.interpret("START auto val1 = 2 + 2 END.");
 
-TEST_CASE("[Interpreter] 2+2") {
-	CodeInterpreter interpreter{};
-	interpreter.interpret("START auto val1 = 2 + 2 END.");
-	
-	CHECK(interpreter.getVariable("val1") == "4");
+    CHECK(interpreter.getVariable("val1") == "4");
 }
 
-
-} 
+} // namespace TestInterpreter
